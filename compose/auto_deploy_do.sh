ba20b484c6c21e90914fef88185b3bb74f529b1e
phat96 <phat@yojee.com>
2022-06-07 10:51:16 +0700

    add supervisor

--- compose/auto_deploy_do.sh
+++ compose/auto_deploy_do.sh
@@ -4,7 +4,7 @@
 # DigitalOcean Droplet
 
 # generate TAR file from git
-git archive --format tar --output ./project.tar main
+git archive --format tar --output ./project.tar master
 
 echo 'Uploading project...'
 rsync ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
@@ -15,5 +15,6 @@ ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
     mkdir -p /app
     rm -rf /app/* && tar -xf /tmp/project.tar -C /app
     docker-compose -f /app/docker-compose.prod.yml build
+    supervisorctl restart fastapi-celery-app
 ENDSSH
 echo 'Build complete.'
